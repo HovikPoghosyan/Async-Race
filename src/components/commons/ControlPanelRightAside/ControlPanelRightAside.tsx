37c69b9656eb68b06351bc1411b59986dae1b8b1
HovikPoghosyan <hovik.poghosyan2005@gmail.com>
2025-10-01 17:32:58 +0400

    fix: fix reset button functionality

--- src/components/commons/ControlPanelRightAside/ControlPanelRightAside.tsx
+++ src/components/commons/ControlPanelRightAside/ControlPanelRightAside.tsx
@@ -11,14 +11,15 @@ import styles from './ControlPanelRightAside.module.scss';
 function ControlPanelRightAside() {
    const dispatch = useAppDispatch();
    const { garageList, loading, race } = useAppSelector((state) => state.list);
+   
    return (
       <aside className={styles.aside}>
          <div className={styles.row}>
             <h2 className={styles.title}>{`GARAGE (count: ${garageList.length})`}</h2>
          </div>
          <div className={styles.row}>
-            <Button name="Race" isDisable={!!(loading || !!race)} functionality={() => dispatch(setRace('started'))} />
-            <Button name="Reset" isDisable={!!(loading || !race)} functionality={() => dispatch(setRace(false))} />
+            <Button name="Race" isDisable={loading || !(!race || race == 'stopped')} functionality={() => dispatch(setRace('started'))} />
+            <Button name="Reset" isDisable={loading || !!(!race || race == 'stopped')} functionality={() => dispatch(setRace('stopped'))} />
             <Button
                name="Generate Car"
                isDisable={!!(loading || race)}
